7642d3527191bd12c8bdbf779aa233121a507461
migrgh <163189276+migrgh@users.noreply.github.com>
2024-05-21 20:28:21 +0200

    Create services.sh

---
+++ services.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+STOP="systemd-resolved systemd-timesyncd systemd-timedated systemd-homed systemd-userdbd systemd-userdbd.socket"
+
+sudo systemctl stop $STOP
+sudo systemctl mask $STOP
+sudo systemctl daemon-reload
