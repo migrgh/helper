d00f09384ceb4d6f46bb74b5d826209f9c9bb32b
migrgh <163189276+migrgh@users.noreply.github.com>
2024-05-21 20:47:10 +0200

    Update fedora-remove-packages.sh

--- fedora-remove-packages.sh
+++ fedora-remove-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo dnf remove NetworkManager firewalld avahi linux-firmware-whence sssd* bluez
+sudo dnf remove NetworkManager firewalld avahi linux-firmware-whence sssd* bluez man-db
 
 sudo dnf install nftables amd-gpu-firmware amd-ucode-firmware
